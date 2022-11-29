1368953a35b318386cfc6b7506d53e66afc1a776
denicem <denicem@hotmail.de>
2022-11-29 14:03:10 +0100

    HttpResponse: implement method to get specific status msg depending on status code and remove var statusMsg; overload operator<<.

--- inc/HttpResponse.hpp
+++ inc/HttpResponse.hpp
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 18:43:33 by dmontema          #+#    #+#             */
-/*   Updated: 2022/11/29 02:37:10 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/11/29 13:51:10 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 class HttpResponse: public HttpMessage {
 	private:
 		int statusCode;
-		std::string statusMsg;
 		File file;
 
 	private:
 		bool isMethodAllowed(const int, const Location&) const;
+		std::string getStatusMsg() const;
 
 	public:
 		HttpResponse();
@@ -38,12 +38,12 @@ class HttpResponse: public HttpMessage {
 		HttpResponse& operator=(const HttpResponse&);
 
 		int getStatusCode() const;
-		std::string getStatusMsg() const;
 
 		void setStatusCode(const int&);
-		void setStatusMsg(const std::string&);
 
 		std::string genHttpResponseMsg(const HttpRequest&) const;
+
+		friend std::ostream& operator<<(std::ostream&, const HttpResponse&);
 };
 
 #endif
--- src/class/HttpResponse.cpp
+++ src/class/HttpResponse.cpp
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 22:44:52 by dmontema          #+#    #+#             */
-/*   Updated: 2022/11/29 02:36:30 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/11/29 13:59:16 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,19 @@
 */
 
 bool HttpResponse::isMethodAllowed(const int method, const Location& location) const {
-	for (unsigned long i = 0; i < location.getAllowedMethods().size(); ++i)
-	{
+	for (unsigned long i = 0; i < location.getAllowedMethods().size(); ++i) {
 		if (location.getAllowedMethods().at(i) == method)
 			return (true);
 	}
 	return (false);
+}
+
+std::string HttpResponse::getStatusMsg() const {
+	switch (this->statusCode) {
+		case 200: return ("OK");
+		case 404: return ("Not Found");
+		default: return ("undefined");
+	}
 }
 
 /*
@@ -33,38 +40,28 @@ bool HttpResponse::isMethodAllowed(const int method, const Location& location) c
 */
 
 HttpResponse::HttpResponse() {}
-HttpResponse::HttpResponse(const HttpResponse& other): HttpMessage(other), statusCode(other.statusCode), statusMsg(other.statusMsg) {}
+HttpResponse::HttpResponse(const HttpResponse& other): HttpMessage(other), statusCode(other.statusCode) {}
 
-HttpResponse::HttpResponse(const std::string& filename)
-{
+HttpResponse::HttpResponse(const std::string& filename) {
 	this->file = File(filename);
 	this->statusCode = 200;
-	this->statusMsg = "OK";
 }
 
-HttpResponse::HttpResponse(const HttpRequest& req)
-{
-	if (req.getHttpMethod() == GET) //TODO: switch case!!
-	{
-		try
-		{
+HttpResponse::HttpResponse(const HttpRequest& req) {
+	if (req.getHttpMethod() == GET) {  //TODO: switch case!!
+		try {
 			this->file = File(req.getURI());
 			this->statusCode = 200;
-			this->statusMsg = "OK";
 		}
-		catch (File::FileNotFoundException& e)
-		{
+		catch (File::FileNotFoundException& e) {
 			this->file = File("/404/404.html");
 			this->statusCode = 404;
-			this->statusMsg = "Not Found";
 		}
 	}
 }
 
-HttpResponse::HttpResponse(const HttpRequest& req, const Server& server)
-{
-	for (unsigned long i = 0; i < server.getLocations().size(); ++i)
-	{
+HttpResponse::HttpResponse(const HttpRequest& req, const Server& server) {
+	for (unsigned long i = 0; i < server.getLocations().size(); ++i) {
 		std::cout << server.getLocation(i) << std::endl;
 		// if ((req.getURI() == server.getLocation(i).getName()) || req.getURI() == "/") {
 		if (req.getURI().find(server.getLocation(i).getName()) != std::string::npos) {
@@ -77,42 +74,32 @@ HttpResponse::HttpResponse(const HttpRequest& req, const Server& server)
 				{
 					try
 					{
-						std::cout << "hehe" << std::endl;
-						// this->file = File(req.getURI());
 						this->file = File(server.getLocation(i).getName(), server, i, true);
 						this->statusCode = 200;
-						this->statusMsg = "OK";
 					}
 					catch (File::FileNotFoundException& e)
 					{
 						this->file = File("/404/404.html");
 						this->statusCode = 404;
-						this->statusMsg = "Not Found";
 					}
 					break ;
 				}
 			// }
 		}
-		else
-		{
+		else {
 			if (req.getHttpMethod() == GET)
 			{
-				try
-				{
+				try {
 					this->file = File(req.getURI());
 					this->statusCode = 200;
-					this->statusMsg = "OK";
 				}
-				catch (File::FileNotFoundException& e)
-				{
+				catch (File::FileNotFoundException& e) {
 					this->file = File("/404/404.html");
 					this->statusCode = 404;
-					this->statusMsg = "Not Found";
 				}
 			}
 		}
 	}
-
 }
 
 HttpResponse::~HttpResponse() {}
@@ -121,16 +108,13 @@ HttpResponse::~HttpResponse() {}
 ** ----------------------- OPERATOR OVERLOADS -----------------------
 */
 
-HttpResponse& HttpResponse::operator=(const HttpResponse& other)
-{
-	if (this != &other)
-	{
+HttpResponse& HttpResponse::operator=(const HttpResponse& other) {
+	if (this != &other) {
 		// this->firstLine = other.firstLine;
 		// this->httpVer = other.httpVer;
 		// this->headers = other.headers;
 		// this->msgBody = other.msgBody; // NOTE: will the operator=() method from the Base class called first?
 		this->statusCode = other.statusCode;
-		this->statusMsg = other.statusMsg;
 	}
 	return (*this);
 }
@@ -139,52 +123,39 @@ HttpResponse& HttpResponse::operator=(const HttpResponse& other)
 ** ----------------------- GETTER AND SETTER METHODS -----------------------
 */
 
-int HttpResponse::getStatusCode() const
-{
+int HttpResponse::getStatusCode() const {
 	return (this->statusCode);
 }
 
-std::string HttpResponse::getStatusMsg() const
-{
-	return (this->statusMsg);
-}
 
-
-void HttpResponse::setStatusCode(const int& statusCode)
-{
+void HttpResponse::setStatusCode(const int& statusCode) {
 	this->statusCode = statusCode;
-}
-
-void HttpResponse::setStatusMsg(const std::string& statusMsg)
-{
-	this->statusMsg = statusMsg;
 }
 
 /*
 ** ----------------------- METHODS -----------------------
 */
 
-std::string HttpResponse::genHttpResponseMsg(const HttpRequest& req) const
-{
+std::string HttpResponse::genHttpResponseMsg(const HttpRequest& req) const {
 	std::stringstream stream;
 
-	stream << "HTTP/1.1 " << std::to_string(this->statusCode) << " " << this->statusMsg << std::endl;
-	stream << "Content-Type: text/html\nContent-Length: " << std::to_string(this->file.getFileSize()) << std::endl;
-	stream << std::endl;
-	if (req.getHttpMethod() == GET)
+	stream << req.getHttpVer() << " " << std::to_string(this->statusCode) << " " << this->getStatusMsg() << std::endl;
+	if (req.getHttpMethod() == GET) {
+		stream << "Content-Type: text/html\nContent-Length: " << std::to_string(this->file.getFileSize()) << std::endl;
+		stream << std::endl;
 		stream << this->file.getContent();
-
+	}
 	return (stream.str());
 }
 
 /*
-** ----------------------- CLASS ATTRIBUTES -----------------------
-*/
-
-/*
-** ----------------------- CLASS METHODS -----------------------
-*/
-
-/*
 ** ----------------------- FUNCS -----------------------
 */
+
+std::ostream& operator<<(std::ostream& stream, const HttpResponse& resp) {
+	stream << "***** RESPONSE *****" << std::endl;
+	stream << "HTTP ver: " << resp.httpVer << std::endl;
+	stream << "Status code: " << resp.statusCode <<  std::endl;
+	stream << "********************" << std::endl;
+	return (stream);
+}
