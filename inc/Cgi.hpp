23d1e2515fe052d1612ed43362fc3456c9664fea
lavendelchen <lavendel02sh@gmail.com>
2022-12-07 22:27:22 +0100

    made isCGI function

--- inc/Cgi.hpp
+++ inc/Cgi.hpp
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cgi.hpp                                            :+:      :+:    :+:   */
+/*   CGI.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:24:27 by shaas             #+#    #+#             */
-/*   Updated: 2022/12/07 17:32:37 by shaas            ###   ########.fr       */
+/*   Updated: 2022/12/07 22:15:25 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Webserv.hpp"
+#include "HttpAction.hpp"
 
 extern char **enviroment;
 
@@ -30,11 +31,17 @@ class CGI
 		int _fd_out[2];
 		
 		CGI() {}
+		CGI(const HttpAction& http);
 		~CGI() {}
 
 		//set enviroment
 		void	setEnv(void);
 
 		void	executeCGI(void);
+
+		/***
+		 * @param allowed_cgi_for_route when NULL, allow all possible cgi
+		*/
+		static bool	isCGI(const string& filename, const vector<string>* allowed_cgi_for_route);
 
 };
--- src/class/CGI.cpp
+++ src/class/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:25:48 by shaas             #+#    #+#             */
-/*   Updated: 2022/12/07 17:29:35 by shaas            ###   ########.fr       */
+/*   Updated: 2022/12/07 22:25:29 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,28 @@
 ** ----------------------- STATIC METHODS -----------------------
 */
 
+bool	CGI::isCGI(const string& filename, const vector<string>* allowed_cgi_for_route)
+{
+	if (filename.rfind('.') == string::npos)
+		return false;
+	string file_extension = filename.substr(filename.rfind('.'));
+	if (allowed_cgi_for_route == NULL)
+	{
+		/////insert global cgi extensions here!!!
+		return false; /// just for now!!!!
+	}
+	for (vector<string>::const_iterator i = allowed_cgi_for_route->cbegin(); i != allowed_cgi_for_route->end(); i++)
+	{
+		if (file_extension == *i)
+			return true;
+	}
+	return false;
+}
+
 /*
 ** ----------------------- OPERATOR OVERLOADS -----------------------
 */
 
 /*
-** ----------------------- CLASS METHODS -----------------------
+** ----------------------- PUBLIC METHODS -----------------------
 */
-
-
