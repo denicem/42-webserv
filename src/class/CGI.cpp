42f9cc44307bb43b4f84f7c4d8901956180da1fc
lavendelchen <lavendel02sh@gmail.com>
2022-12-15 19:31:06 +0100

    root or alias -> only root directory

--- src/class/CGI.cpp
+++ src/class/CGI.cpp
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:25:48 by shaas             #+#    #+#             */
-/*   Updated: 2022/12/11 20:49:03 by shaas            ###   ########.fr       */
+/*   Updated: 2022/12/15 19:17:36 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,40 +27,40 @@ void	CGI::CGIError(string error_msg)
 ** ----------------------- PRIVATE METHODS -----------------------
 */
 
-string	CGI::executeCGI(void)
-{
-	string	compile_cmd;
+// string	CGI::executeCGI(void)
+// {
+// 	string	compile_cmd;
 
-	compile_cmd = "gcc ./cgi-bin/" + _program_name + " -o cgi_prg";
-	int pid;
-	int	pp[2];
-	if (pipe(pp) == -1)
-		CGIError("Error when opening pipe");
-	pid = fork();
-	if (pid == 0)
-	{
-		dup2(pp[1], STDOUT_FILENO);
-		close(pp[1]);
-		close(pp[0]);
-		if (execl("/usr/bin/perl", "perl", phpfile.c_str(), NULL) == -1 )
-			std::cerr << "Error in perl cgi Execution\n";
-		exit(0);
-	}
-	else if (pid == -1)
-		CGIError("Fork has failed");
-	else
-	{
-		waitpid(-1, NULL, 0);
-		char hold;
-		fcntl(fd[0], F_SETFL, O_NONBLOCK);
-		while (read(fd[0], &hold, 1) != -1)
-			temp += hold;
-		close(fd[1]);
-		close(fd[0]);
-	}
-	return (temp);
-	}
-}
+// 	compile_cmd = "gcc ./cgi-bin/" + _program_name + " -o cgi_prg";
+// 	int pid;
+// 	int	pp[2];
+// 	if (pipe(pp) == -1)
+// 		CGIError("Error when opening pipe");
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		dup2(pp[1], STDOUT_FILENO);
+// 		close(pp[1]);
+// 		close(pp[0]);
+// 		if (execl("/usr/bin/perl", "perl", phpfile.c_str(), NULL) == -1 )
+// 			std::cerr << "Error in perl cgi Execution\n";
+// 		exit(0);
+// 	}
+// 	else if (pid == -1)
+// 		CGIError("Fork has failed");
+// 	else
+// 	{
+// 		waitpid(-1, NULL, 0);
+// 		char hold;
+// 		fcntl(fd[0], F_SETFL, O_NONBLOCK);
+// 		while (read(fd[0], &hold, 1) != -1)
+// 			temp += hold;
+// 		close(fd[1]);
+// 		close(fd[0]);
+// 	}
+// 	return (temp);
+// 	}
+// }
 
 /*
 ** ----------------------- CONSTRUCTORS & DESTRUCTOR -----------------------
