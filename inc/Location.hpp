9f215650615538ec84d6f61aba63ff9d58419eac
denicem <denicem@hotmail.de>
2022-12-17 16:58:58 +0100

    remove path variable from class Location.

--- inc/Location.hpp
+++ inc/Location.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Location.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjeyavat <mjeyavat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 18:37:57 by mjeyavat          #+#    #+#             */
-/*   Updated: 2022/12/15 19:23:24 by mjeyavat         ###   ########.fr       */
+/*   Updated: 2022/12/17 16:57:03 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,18 @@
 class Location {
 	private:
 		std::string name;
-		std::string root; // root or alias
-		std::string path; // root (from Server?) + this->name OR  if isAlias == true -> root
+		std::string root;
 		std::string index; //default_file
 		std::vector<HttpMethod>allowedMethods;
-		bool isAlias;
 		bool directoryListing;
 
 	public:
-		Location(const std::string& name, const std::string& rootOrAlias, 
+		Location(const std::string& name, const std::string& root, 
 			const std::string& index, const std::vector<HttpMethod>&, bool directoryListing);
 
 		std::string getName() const;
-		std::string getPath() const;
 		std::string getIndex() const;
-
+		std::string getRoot() const;
 		std::vector<HttpMethod>getAllowedMethods() const;
 
 		friend std::ostream& operator<<(std::ostream&, const Location&);
