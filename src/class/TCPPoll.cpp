770b067068c99f354a5d6a60a1eb1780031b1830
Denice M <denicem@hotmail.de>
2022-12-21 12:53:39 +0100

    modify some files for WSL.

--- src/class/TCPPoll.cpp
+++ src/class/TCPPoll.cpp
@@ -77,6 +77,7 @@ void TCPPoll::status_check()
 				break;
 			case POLL_ERR:
 				cout << "Error on poll" << endl;
+				break ;
 			default:
 				for (index = 0; index < getMaxConnection(); index++)
 				{
