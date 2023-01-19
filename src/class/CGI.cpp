97c6677128d304270bc4bc32b88ee009f46aa7da
Lavendelchen <lavendel02sh@gmail.com>
2023-01-19 20:43:32 +0100

    trying to get cgi to work

--- src/class/CGI.cpp
+++ src/class/CGI.cpp
@@ -28,6 +28,29 @@ void	CGI::CGIError(string error_msg)
 	throw CGIException();
 }
 
+char**	CGI::envConverter(const vector<string>& env)
+{
+	char** c_env;
+
+	c_env = (char **)malloc(sizeof(char *) * (env.size() + 1));
+	if (c_env == NULL)
+		CGIError("Could not allocate memory");
+	c_env[env.size()] = NULL;
+	for (unsigned long i = 0; i < env.size(); i++)
+	{
+		c_env[i] = strdup(env[i].c_str());
+		if (c_env[i] == NULL)
+			CGIError("Could not allocate memory");
+	}
+	cout << LIGHTBLUE << "ENV\n" << RESET; 
+	for (int i = 0; c_env[i] != NULL; i++)
+	{
+		cout << c_env[i] << '\n';
+	}
+	cout << LIGHTBLUE << "ENV END\n\n" << RESET; 
+	return (c_env);
+}
+
 string	CGI::intToString(int i)
 {
 	stringstream ss;
@@ -41,40 +64,48 @@ string	CGI::intToString(int i)
 
 void	CGI::setEnv(void)
 {
-	vector<string>	prepare_env;
+	vector<string>	_env;
+	string			buf;
 
-	prepare_env.push_back("SERVER_SOFTWARE=DMS-Server/0.5");
-	prepare_env.push_back("SERVER_NAME=" + _server_name);
-	prepare_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
-	prepare_env.push_back("SERVER_PORT=" + intToString(_server_port));
-	prepare_env.push_back("SERVER_PROTOCOL=HTTP/1.1");
-	prepare_env.push_back("REQUEST_METHOD=" + _request_method);
-	prepare_env.push_back("PATH_INFO=" + _path_info);
-	prepare_env.push_back("PATH_TRANSLATED=" + _path_info);
-	prepare_env.push_back("SCRIPT_NAME=" + _script_name);
-	prepare_env.push_back("QUERY_STRING=" + _query_string);
-	prepare_env.push_back("CONTENT_TYPE=" + _content_type);
-	prepare_env.push_back("CONTENT_LENGTH=" + _content_length);
+	_env.push_back("SERVER_SOFTWARE=DMS-Server/1.0");
+	_env.push_back("GATEWAY_INTERFACE=CGI/1.1");
+	_env.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	_env.push_back("REQUEST_METHOD=GET"); //because we only allow cgi for GET
+	_env.push_back("PATH_INFO=");
+	_env.push_back("PATH_TRANSLATED=");
+	_env.push_back("QUERY_STRING=" + _query_string);
+	_env.push_back("SCRIPT_NAME=" + _program_name);
+	if (_request_headers.find("Host") != _request_headers.end()) {
+		buf = _request_headers.find("Host")->second;
+		_server_name = buf.substr(0, buf.find(':') == string::npos ? buf.size() : buf.find(':'));
+		_request_headers.erase("Host");
+	}
+	_env.push_back("SERVER_NAME=" + _server_name);
+	_env.push_back("SERVER_PORT=" + intToString(_server_port));
+	_env.push_back("REMOTE_ADDR=");
 
-	_env = (char **)malloc(sizeof(char *) * (prepare_env.size() + 1));
-	if (_env == NULL)
-		CGIError("Could not allocate memory");
-	_env[prepare_env.size()] = NULL;
-	for (unsigned long i = 0; i < prepare_env.size(); i++)
-	{
-		_env[i] = strdup(prepare_env[i].c_str());
-		if (_env[i] == NULL)
-			CGIError("Could not allocate memory");
+	for (map<string, string>::iterator i = _request_headers.begin(); i != _request_headers.end(); i++) {
+		buf = i->first;
+		for (string::iterator j = buf.begin(); j != buf.end(); j++) {
+			*j = toupper(*j);
+			if (*j == '-')
+				*j = '_';
+		}
+		_env.push_back("HTTP_" +buf + '=' + i->second);
 	}
-	cout << LIGHTBLUE << "ENV\n" << RESET; 
-	for (int i = 0; _env[i] != NULL; i++)
+
+	cout << LIGHTCYAN << "ENV\n" << RESET; 
+	for (vector<string>::iterator i = _env.begin();i != _env.end(); i++)
 	{
-		cout << _env[i] << '\n';
+		cout << *i << '\n';
 	}
+	cout << LIGHTBLUE << "ENV END\n\n" << RESET; 
 }
 
-string	CGI::executeCGI(void)
+void	CGI::executeCGI(void)
 {
+	char**	env = envConverter(_env);
+
 	int 	pid;
 	int		cgi_out[2];
 	int		cgi_in[2];
@@ -94,11 +125,11 @@ string	CGI::executeCGI(void)
 		dup2(cgi_in[READ], STDIN_FILENO);
 		close(cgi_in[READ]);
 		if (_cgi_extension == ".cgi") {
-			if (execle(("./cgi-bin/" + _program_name).c_str(), _program_name.c_str(), (char*)NULL, _env) == -1)
+			if (execle(_program_name.c_str(), _program_name.c_str(), (char*)NULL, env) == -1)
 				CGIError("Could not execute binary CGI program");
 		}
 		else if (_cgi_extension == ".py") {
-			if (execle("/usr/bin/python3", "python3", ("./cgi-bin/" + _program_name).c_str(), (char*)NULL, _env) == -1)
+			if (execle("/usr/bin/python3", "python3", _program_name.c_str(), (char*)NULL, env) == -1)
 				CGIError("Could not execute Python CGI program");
 		}
 		else
@@ -117,28 +148,48 @@ string	CGI::executeCGI(void)
 		close(cgi_out[READ]);
 		close(cgi_out[WRITE]);
 		close(cgi_in[READ]);
+
+		for (int i = 0; env[i] != NULL; i++)
+		{
+			free(env[i]);
+		}
+		free(env);
+
+		if (cgi_out_str.find("\r\n\r\n") == string::npos) {
+			_response_type = "plain";
+			_response_body = cgi_out_str;
+		}
+		else {
+			_response_type = cgi_out_str.substr(0, cgi_out_str.find("\r\n\r\n"));
+			if (_response_type.find("Content-type:") == string::npos)
+				_response_type = "plain";
+			else {
+				_response_type = _response_type.substr(_response_type.find("Content-type:")+13);
+				_response_type = _response_type.substr(_response_type.find("/")+1); //
+				_response_type = _response_type.substr(0, _response_type.find("\n")+1);
+			}
+			_response_body = cgi_out_str.substr(cgi_out_str.find("\r\n\r\n")+4);
+		}
 	}
-	return (cgi_out_str);
 }
 
 /*
 ** ----------------------- CONSTRUCTORS & DESTRUCTOR -----------------------
 */
 
-CGI::CGI()
+CGI::CGI(const map<string, string>& request_headers, int port,
+		const string& query_string, const string& cgi_file,
+		const string& http_version)
 {
-	_program_name = "telephone.cgi";
-	_cgi_extension = ".cgi";
-	this->setEnv(); // temporary
-}
-
-CGI::~CGI() // TEST!!
-{
-	for (int i = 0; _env[i] != NULL; i++)
-	{
-		free(_env[i]);
-	}
-	free(_env);
+	if (access(("./cgi-bin/" + cgi_file).c_str(), F_OK) == -1)
+		throw File::FileNotFoundException();
+	_program_name = "./cgi-bin/" + cgi_file;
+	_cgi_extension = cgi_file.substr(cgi_file.rfind('.'));
+	_server_port = port;
+	_query_string = query_string;
+	_server_protocol = http_version;
+	_request_headers = request_headers;
+	this->setEnv();
 }
 
 /*
@@ -163,6 +214,23 @@ bool	CGI::isCGI(const string& filename, const vector<string>& allowed_cgi_for_ro
 /*
 ** ----------------------- OPERATOR OVERLOADS -----------------------
 */
+
+CGI&	CGI::operator=(const CGI& rhs)
+{
+	if (this != &rhs) {
+		this->_program_name = rhs._program_name;
+		this->_cgi_extension = rhs._cgi_extension;
+		this->_request_headers = rhs._request_headers;
+		this->_server_port = rhs._server_port;
+		this->_query_string = rhs._query_string;
+		this->_server_protocol = rhs._server_protocol;
+		this->_server_name = rhs._server_name;
+		this->_env = rhs._env;
+		this->_response_type = rhs._response_type;
+		this->_response_body = rhs._response_body;
+	}
+	return *this;
+}
 
 /*
 ** ----------------------- PUBLIC METHODS -----------------------
